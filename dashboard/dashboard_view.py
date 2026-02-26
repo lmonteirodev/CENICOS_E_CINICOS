326c58b1c36d5289ac444a458c9561f935b48313
The-Atlas-Dev <atlas.office642@gmail.com>
2026-02-26 08:21:49 -0300

    .Venv SQL and PYQT5 install

--- dashboard/dashboard_view.py
+++ dashboard/dashboard_view.py
@@ -1,2 +1,11 @@
 # View do módulo Dashboard
 # Responsável pela visualização e interação com o usuário
+from dashboard.dasboard
+
+class DashboardView:
+    def __init__(self):
+        pass
+
+    def display_dashboard(self, ui_main_window):
+        # Exibe o dashboard usando a interface do usuário fornecida
+        ui_main_window.show()
