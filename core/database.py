d73e2c360a9a3f5ce31dcb7c3b6d9b8326066d54
Engsoftlucas <eng.soft.lucast@gmail.com>
2026-02-27 10:25:09 -0300

    Update database.py

--- core/database.py
+++ core/database.py
@@ -10,28 +10,28 @@ conexao = mysql.connector.connect(
 )
 cursor = conexao.cursor()
 
-# ------------------------------------------------ CRUD ---------------------------------------------------------
-# ----------------- CREAT
-comando = 'INSERT INTO cliente (nome, cpf, cnpj, tipo_pessoa) VALUES ("Matheus", "01567911293", "null", "F")'
-cursor.execute(comando)
-conexao.commit()
+# # ------------------------------------------------ CRUD ---------------------------------------------------------
+# # ----------------- CREAT
+# comando = 'INSERT INTO cliente (nome, cpf, cnpj, tipo_pessoa) VALUES ("Matheus", "01567911293", "null", "F")'
+# cursor.execute(comando)
+# conexao.commit()
 
-# # ------------------READ
-comando = 'SELECT * FROM cliente'
-cursor.execute(comando)
-resultado = cursor.fetchall()
-print(resultado)
+# # # ------------------READ
+# comando = 'SELECT * FROM cliente'
+# cursor.execute(comando)
+# resultado = cursor.fetchall()
+# print(resultado)
 
-# ---------------- UPDATE
-comando = 'UPDATE cliente SET cnpj = 1 WHERE ID_cliente = 1'
-cursor.execute(comando)
-conexao.commit()
+# # ---------------- UPDATE
+# comando = 'UPDATE cliente SET cnpj = 1 WHERE ID_cliente = 1'
+# cursor.execute(comando)
+# conexao.commit()
 
-# -------------- DELETE
-comando = 'DELETE FROM cliente WHERE ID_cliente = 3'
-cursor.execute(comando)
-conexao.commit()
+# # -------------- DELETE
+# comando = 'DELETE FROM cliente WHERE ID_cliente = 3'
+# cursor.execute(comando)
+# conexao.commit()
 
 
-cursor.close()
-conexao.close()
+# cursor.close()
+# conexao.close()
