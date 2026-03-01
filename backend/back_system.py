beacb6397dd5c4a6407b989f6bfe58c927959968
Lucas Monteiro <eng.soft.lucast@gmail.com>
2026-03-01 07:19:11 -0300

    try add database

--- backend/back_system.py
+++ backend/back_system.py
@@ -1,10 +1,11 @@
 # IMPORTS----------------------------------------------------------------------------------------------------------
-# from core.database import get_connection
+from database import get_connection
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
-from reportlab.pdfgen import canvas
+from PyQt5 import uic, QtWidgets
+# from reportlab.pdfgen import canvas
 import sys
-import os
+# import os
+import re
 
 
 # OBJETOS-------------------------------------------------------------------------------------------------------------
@@ -88,6 +89,50 @@ class ClientesCadScreen(BaseScreen):
         # frame superior
         self.pushButton_2.clicked.connect(lambda: self.controller.show_screen(ClientesScreen))
 
+    def cadastrar_cliente ():
+        try:
+            conexao = get_connection()
+            if not conexao:
+                QtWidgets.QMessageBox.critical(ClientesCadScreen, "Erro", "Sem conexão com banco de dados.")
+                return
+            
+            def definir_tipo_pessoa (cpf_cnpj: str):
+                somente_numeros = re.sub(r'\D', '', cpf_cnpj)
+                if len(somente_numeros) == 11:
+                    return 'F'
+                elif len(somente_numeros) == 14:
+                    return 'J' 
+                else: raise ValueError("CPF/CNPJ inválido")
+
+            nome = ClientesCadScreen.lineEdit_1.text().strip()
+            cpf_cnpj = ClientesCadScreen.lineEdit_2.text().strip()
+            telefone = ClientesCadScreen.lineEdit_3.text().strip()
+            email = ClientesCadScreen.lineEdite_4.text().strip()
+
+            if not (nome and cpf_cnpj and telefone and email):
+                QtWidgets.QMessageBox.warning(ClientesCadScreen, "Aviso", "Preencha todos os campos.")
+                conexao.close()
+                return
+            
+            tipo_pessoa = definir_tipo_pessoa()
+        
+            cursor = conexao.cursor()
+            sql = """INSERT INTO cliente (nome, cpf_cnpj, tipo_pessoa) VALUES (%s, %s, %s)"""
+            sql2 = """INSERT INTO telefone_cliente (telefone) VALUES (%s)"""
+            sql3 = """INSERT INTO email_cliente (email) VALUES (%s)"""
+            valores = (nome, cpf_cnpj, telefone, email, tipo_pessoa)
+
+            cursor.execute(sql, sql2, sql3, valores)
+            conexao.commit()
+            cursor.close()
+            conexao.close()
+
+            QtWidgets.QMessageBox.information(ClientesCadScreen, "Sucesso", "Cliente cadastrado com sucesso!")
+
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(ClientesCadScreen, "Erro", f"Ocorreu um erro: {str(e)}")
+
+
 class FuncScreen(BaseScreen):
     def __init__(self, controller):
         super().__init__("telas/aba_funcionarios.ui")
@@ -134,4 +179,5 @@ if __name__ == "__main__":
     controller = ScreenController()
     controller.show_screen(DashScreen)
 
+    # ClientesCadScreen.pushButton_3.clicked.connect(ClientesCadScreen.cadastrar_cliente)
     sys.exit(app.exec())
