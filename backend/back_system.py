a35133a42ab58fb69e2d819320790f965682549a
Lucas Monteiro <eng.soft.lucast@gmail.com>
2026-03-04 01:22:39 -0300

    Database insert, join values

--- backend/back_system.py
+++ backend/back_system.py
@@ -74,6 +74,50 @@ class ClientesScreen(BaseScreen):
         # frame superior
         self.pushButton.clicked.connect(lambda: self.controller.show_screen(ClientesCadScreen))
 
+        self.tableWidget.setColumnCount(5)
+        self.tableWidget.setHorizontalHeaderLabels(
+            ["Cliente", "Documento", "Contato", "Status", "Serviços"]
+        )
+
+        header = self.tableWidget.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)           # Cliente
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)  # Documento
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)           # Contato
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.ResizeToContents)  # Status
+        header.setSectionResizeMode(4, QtWidgets.QHeaderView.ResizeToContents)  # Serviços
+
+
+        self.carregar_clientes()
+
+    def carregar_clientes(self):
+        conexao = get_connection()
+        cursor = conexao.cursor()
+
+        cursor.execute("""
+            SELECT c.nome, c.cpf_cnpj, t.telefone, e.email
+            FROM cliente c
+            LEFT JOIN telefone_cliente t ON t.ID_cliente = c.ID_cliente
+            LEFT JOIN email_cliente e ON e.ID_cliente = c.ID_cliente
+        """)
+        clientes = cursor.fetchall()
+
+        self.tableWidget.setRowCount(len(clientes))
+
+        for i, (nome, documento, telefone, email) in enumerate(clientes):
+            contato = f"{email}, {telefone}" if email and telefone else (email or telefone or "N/A")
+
+            self.tableWidget.setItem(i, 0, QtWidgets.QTableWidgetItem(nome))
+            self.tableWidget.setItem(i, 1, QtWidgets.QTableWidgetItem(documento))
+            self.tableWidget.setItem(i, 2, QtWidgets.QTableWidgetItem(contato))
+            self.tableWidget.setItem(i, 3, QtWidgets.QTableWidgetItem("ATIVO"))  # ou N/A se não tiver status
+            self.tableWidget.setItem(i, 4, QtWidgets.QTableWidgetItem("0"))      # número de serviços
+
+        cursor.close()
+        conexao.close()
+
+
+        
+
 class ClientesCadScreen(BaseScreen):
     def __init__(self, controller):
         super().__init__("telas/aba_clientes_cad.ui")
@@ -147,6 +191,11 @@ class ClientesCadScreen(BaseScreen):
 
             QtWidgets.QMessageBox.information(self, "Sucesso", "Cliente cadastrado com sucesso!")
 
+            self.lineEdit_1.clear()
+            self.lineEdit_2.clear()
+            self.lineEdit_3.clear()
+            self.lineEdit_4.clear()
+
         except Exception as e:
             QtWidgets.QMessageBox.critical(self, "Erro", f"Ocorreu um erro: {str(e)}")
 
@@ -165,6 +214,7 @@ class FuncScreen(BaseScreen):
         # frame superior
         self.pushButton.clicked.connect(lambda: self.controller.show_screen(FuncCadScreen))
 
+
 class FuncCadScreen(BaseScreen):
     def __init__(self, controller):
         super().__init__("telas/aba_funcionarios_cad.ui")
@@ -179,6 +229,69 @@ class FuncCadScreen(BaseScreen):
 
         # frame superior
         self.pushButton_2.clicked.connect(lambda: self.controller.show_screen(FuncScreen))
+
+        # Botão de cadastro
+        self.pushButton_3.clicked.connect(self.cadastrar_func)
+        self.pushButton_4.clicked.connect(lambda: self.controller.show_screen(FuncScreen))
+
+    def cadastrar_func (self):
+        try:
+            conexao = get_connection()
+            if not conexao:
+                QtWidgets.QMessageBox.critical(self, "Erro", "Sem conexão com banco de dados.")
+                return
+            
+            def definir_tipo_pessoa (cpf_cnpj: str):
+                somente_numeros = re.sub(r'\D', '', cpf_cnpj)
+                if len(somente_numeros) == 11:
+                    return 'F'
+                elif len(somente_numeros) == 14:
+                    return 'J' 
+                else: raise ValueError("CPF/CNPJ inválido")
+
+            nome = self.lineEdit_1.text().strip()
+            cpf_cnpj = self.lineEdit_2.text().strip()
+            cargo = self.lineEdit_3.text().strip()
+            telefone = self.lineEdit_4.text().strip()
+            email = self.lineEdit_5.text().strip()
+
+            if not (nome and cpf_cnpj and telefone and email and cargo):
+                QtWidgets.QMessageBox.warning(self, "Aviso", "Preencha todos os campos.")
+                conexao.close()
+                return
+            
+            tipo_pessoa = definir_tipo_pessoa(cpf_cnpj)
+        
+            cursor = conexao.cursor()
+
+            # Inserir funcuinario
+            sql_funcionario = """INSERT INTO funcionario (nome, cpf_cnpj, tipo_pessoa, cargo, email) VALUES (%s, %s, %s, %s, %s)"""
+            valores_funcionario = (nome, cpf_cnpj, tipo_pessoa, cargo, email)
+            cursor.execute(sql_funcionario, valores_funcionario)
+
+            # Obter o ID do funcionario inserido
+            ID_funcionario = cursor.lastrowid
+
+            # Inserir telefone vinculado ao funcionario
+            sql_telefone = """INSERT INTO telefone_funcionario (telefone, iD_funcionario) VALUES (%s, %s)"""
+            valores_telefone = (telefone, ID_funcionario)
+            cursor.execute(sql_telefone, valores_telefone)
+
+            # Finalizar
+            conexao.commit()
+            cursor.close()
+            conexao.close()
+
+            QtWidgets.QMessageBox.information(self, "Sucesso", "Cliente cadastrado com sucesso!")
+            
+            self.lineEdit_1.clear()
+            self.lineEdit_2.clear()
+            self.lineEdit_3.clear()
+            self.lineEdit_4.clear()
+            self.lineEdit_5.clear()
+
+        except Exception as e:
+            QtWidgets.QMessageBox.critical(self, "Erro", f"Ocorreu um erro: {str(e)}")
 
 class DocScreen (BaseScreen):
     def __init__(self, controller):
--- backend/database.py
+++ backend/database.py
@@ -4,38 +4,23 @@ import mysql.connector
 def get_connection():
     try:
         conexao = mysql.connector.connect(
-            host="localhost",       # ou IP do servidor
+            host="localhost",
             user="root",
             password="0709",
             database="db_cenicosecinicos"
         )
+        print("Conectado com sucesso!")
         return conexao
+    
     except Exception as e:
         print("Erro ao conectar:", e)
         return None
 
-
-
-# # ------------------------------------------------ CRUD ---------------------------------------------------------
-# # ----------------- CREAT
-# comando = 'INSERT INTO cliente (nome, cpf, cnpj, tipo_pessoa) VALUES ("Matheus", "01567911293", "null", "F")'
-# cursor.execute(comando)
-# conexao.commit()
-
-# # # ------------------READ
-# comando = 'SELECT * FROM cliente'
-# cursor.execute(comando)
-# resultado = cursor.fetchall()
-# print(resultado)
-
-# # ---------------- UPDATE
-# comando = 'UPDATE cliente SET cnpj = 1 WHERE ID_cliente = 1'
-# cursor.execute(comando)
-# conexao.commit()
-
-# # -------------- DELETE
-# comando = 'DELETE FROM cliente WHERE ID_cliente = 3'
-# cursor.execute(comando)
-# conexao.commit()
-
-
+if __name__ == "__main__":
+    conexao = get_connection()
+    
+    if conexao:
+        print("Conexão OK!")
+        conexao.close()
+    else:
+        print("Falha na conexão.")
